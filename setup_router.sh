f179ab4f8a92e420b460d95d78b805ea2ec703a8
EL BOSSO <jkey@arcor.de>
2019-07-13 12:03:56 +0200

    fixes #2

--- setup_router.sh
+++ setup_router.sh
@@ -7,6 +7,7 @@ extdev=${2}
 intdev=${3}
 intaddress=${4}
 intmask=${5}
+nameserver=${6}
 
 echo "operating from within ${script_dir}"
 
@@ -20,6 +21,7 @@ echo "lxc.net.1.link = ${intdev}" >> /var/lib/lxc/${container}/config
 echo "lxc.net.1.flags = up" >> /var/lib/lxc/${container}/config
 
 lxc-start -n ${container}
+sleep 4
 
 lxc-attach -n ${container} -- apt-get -y remove netplan
 
@@ -37,42 +39,44 @@ cp ${script_dir}/interfaces.work ${rootfs}/etc/network/interfaces
 
 lxc-stop -n ${container}
 lxc-start -n ${container}
+sleep 4
+
+lxc-attach -n ${container} -- systemctl stop systemd-resolved.service
+lxc-attach -n ${container} -- systemctl disable systemd-resolved.service
+lxc-attach -n ${container} -- rm /etc/resolv.conf
+lxc-attach -n ${container} -- /bin/bash -c "echo 'nameserver ${nameserver}' >/etc/resolv.conf"
+#lxc-attach -n ${container} -- 
 
 lxc-attach -n ${container} -- apt-get update
 lxc-attach -n ${container} -- apt-get -y upgrade
-lxc-attach -n ${container} -- apt-get -y install joe screen conky ifupdown kea-dhcp4-server iptables unbound
+lxc-attach -n ${container} -- apt-get -y install joe screen conky ifupdown dnsmasq iptables
 
 intsubnet=`echo ${intaddress} | cut -d"." -f1-3`
 
-cp ${script_dir}/kea-dhcp4.conf ${script_dir}/kea-dhcp4.conf.work
-sed -i "s/intsubnet/${intsubnet}/g" ${script_dir}/kea-dhcp4.conf.work
+cp ${script_dir}/dnsmasq.conf ${script_dir}/dnsmasq.conf.work
+sed -i "s/intsubnet/${intsubnet}/g" ${script_dir}/dnsmasq.conf.work
 
-cp ${script_dir}/kea-dhcp4.conf.work ${rootfs}/etc/kea/kea-dhcp4.conf
+cp ${script_dir}/dnsmasq.conf.work ${rootfs}/etc/dnsmasq.conf
 
-lxc-attach -n ${container} -- systemctl enable kea-dhcp4-server
-
-lxc-attach -n ${container} -- service kea-dhcp4-server stop
-lxc-attach -n ${container} -- service kea-dhcp4-server start
-
-cp ${script_dir}/router.conf ${script_dir}/router.conf.work
-sed -i "s/intsubnet/${intsubnet}/g" ${script_dir}/router.conf.work
-
-cp ${script_dir}/router.conf.work ${rootfs}/etc/unbound/unbound.conf.d/router.conf
-
-lxc-attach -n ${container} -- service unbound stop
-lxc-attach -n ${container} -- service unbound start
+lxc-attach -n ${container} -- systemctl stop systemd-resolved.service
+lxc-attach -n ${container} -- systemctl disable systemd-resolved.service
+lxc-attach -n ${container} -- service dnsmasq stop
+lxc-attach -n ${container} -- service dnsmasq start
 
 lxc-attach -n ${container} -- iptables -A FORWARD -o eth0 -i eth1 -s ${intsubnet}.0/24 -m conntrack --ctstate NEW -j ACCEPT
 lxc-attach -n ${container} -- iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 lxc-attach -n ${container} -- iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 #lxc-attach -n ${container} -- 
 
-lxc-attach -n ${container} -- bash -c "echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections"
-lxc-attach -n ${container} -- bash -c "echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections"
+lxc-attach -n ${container} -- /bin/bash -c "echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections"
+lxc-attach -n ${container} -- /bin/bash -c "echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections"
 
 lxc-attach -n ${container} -- apt-get install -y iptables-persistent
 lxc-attach -n ${container} -- apt-get -y autoremove
 lxc-attach -n ${container} -- apt-get clean
 
+lxc-stop -n ${container}
+sleep 4
+lxc-start -n ${container}
 
 
